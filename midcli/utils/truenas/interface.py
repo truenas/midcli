4c1d4d18b7d83a34453e2d760dfb2f9c3092738b
themylogin <themylogin@gmail.com>
2023-01-16 17:21:10 +0100

    Address review

--- midcli/utils/truenas/interface.py
+++ midcli/utils/truenas/interface.py
@@ -8,15 +8,21 @@ __all__ = ["alias_to_str", "str_to_alias"]
 
 
 def alias_to_str(alias, netmask=True):
-    return (
-        alias["address"]
-        if not netmask or {"INET": 32, "INET6": 128}[alias["type"]] == alias["netmask"]
-        else f"{alias['address']}/{alias['netmask']}"
-    )
+    if netmask:
+        return f"{alias['address']}/{alias['netmask']}"
+    else:
+        return alias["address"]
 
 
 def str_to_alias(alias, netmask=True):
     ip_network = ipaddress.ip_network(alias, strict=False)
+
+    if netmask:
+        if "/" not in alias:
+            raise ValueError("Please specify the IP address in CIDR notation")
+    else:
+        if "/" in alias:
+            raise ValueError("Please specify single IP address")
 
     if isinstance(ip_network, ipaddress.IPv4Network):
         result = {
